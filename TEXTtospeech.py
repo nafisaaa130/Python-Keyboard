5cd3f168875f169170531411cee3d04b91b91242
NewCodingPixel <45088282+nafisaaa130@users.noreply.github.com>
2019-01-16 15:58:01 -0500

    Update TEXTtospeech.py

--- TEXTtospeech.py
+++ TEXTtospeech.py
@@ -2,7 +2,7 @@ import pyttsx3;
 engine = pyttsx3.init();
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
-engine.say("Are you okay");
+engine.say("Can you her my voice?");
 engine.runAndWait();
 
 
